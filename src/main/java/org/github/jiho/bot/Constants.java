ca45cb433710627ad2f4664e63eaaf9fe4b5c140
josh971230 <brionlee97@naver.com>
2024-09-03 03:17:42 +0900

    add constants template

---
+++ src/main/java/org/github/jiho/bot/Constants.java
@@ -0,0 +1,9 @@
+package org.github.jiho.bot;
+
+public final class Constants {
+    private Constants() {}
+
+  public static final String API_KEY = "";
+  public static final String BOT_TOKEN = "";
+
+}
