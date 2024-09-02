9f0c7eb8ab501bebda7b51df1b8762887c6e5676
josh971230 <brionlee97@naver.com>
2024-09-03 03:14:07 +0900

    add build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.dv8tion:JDA:5.1.0")
+    implementation ("com.google.code.gson:gson:2.8.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
